52db6044b76963f317c78181a67618669c659e26
DinaMazur <146865201+DinaMazur@users.noreply.github.com>
2024-10-14 18:59:44 +0300

    Add files via upload

---
+++ hw2.js
@@ -0,0 +1,50 @@
+
+let temperatureCelsius = 25;
+let temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
+console.log(temperatureCelsius);
+console.log(temperatureFahrenheit);
+
+let dayMonth = 30;
+let hoursMonth = dayMonth * 24;
+let minMonth = hoursMonth * 60;
+console.log(hoursMonth);
+console.log(minMonth);
+
+const healthy = 1000;
+const energy = 1000; 
+const differenceHealty = healthy - 200;
+const differenceEnergy = energy - 300;
+console.log(differenceHealty);
+console.log(differenceEnergy);
+
+const sum = 5500;
+ const discount = 0.10;
+ const discountResult = sum * (1 - discount); 
+ console.log(discountResult);
+
+
+ const number = 97.51;
+ const numGo = Math.floor(number);
+console.log(numGo);
+
+
+
+const width = "86.12";
+const widthResult = Number.parseFloat(width)
+console.log(widthResult);
+
+
+const height = "15.32";
+// console.log(Number(height)); 
+console.log(Number.parseInt(height)); 
+
+
+const fraction = "9.2";
+// console.log(Number(fraction)); 
+console.log(Number.parseInt(fraction)); 
+
+
+
+console.log(Math);
+const rootNumber = Math.sqrt(16);
+console.log(rootNumber);
